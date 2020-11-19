57825447e1cc7cf03b9a36494f2435ec548a8ce5
lucaz <lucazdenadai@hotmail.com>
2020-11-19 14:47:42 -0300

    ajustes

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.use(bodyParser.json());
 
 //definindo as rotas
 const router = express.Router();
+const cors = require('cors');
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 app.use('/', router);
